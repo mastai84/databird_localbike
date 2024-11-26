a17a76c78088448f45a75a6c14c386beb466cf79
Sami AAN <aitsam84@hotmail.fr>
2024-11-26 17:43:36 +0000

    update store performance

--- models/mart/operations/mrt_operations__store_performance.sql
+++ models/mart/operations/mrt_operations__store_performance.sql
@@ -1,13 +1,13 @@
-select o.store_id, 
-s.store_name,
-sum(o.order_id) as total_orders,
-sum(o.total_item_quantity) as total_items_quantity,
-avg(o.total_item_quantity) as average_item_quantity,
-sum(o.order_amount) as total_order_item_amount,
-avg(o.order_amount) as average_order_amount,
-sum(distinct s.product_id) as total_distinct_products_available
- 
-from {{ ref("int_databird_localbike__order") }} o 
-left join {{ ref("int_databird_localbike__stocks")}} s on o.store_id = s.store_id
-group by store_id,
-store_name
+select
+    o.store_id,
+    s.store_name,
+    sum(o.order_id) as total_orders,
+    sum(o.total_item_quantity) as total_items_quantity,
+    avg(o.total_item_quantity) as average_item_quantity,
+    sum(o.order_amount) as total_order_item_amount,
+    avg(o.order_amount) as average_order_amount,
+    sum(distinct s.product_id) as total_distinct_products_available
+
+from {{ ref("int_databird_localbike__order") }} o
+left join {{ ref("int_databird_localbike__stocks") }} s on o.store_id = s.store_id
+group by store_id, store_name
